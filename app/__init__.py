4317a3a4a1645953a80d8d6c27045e80701b86ec
Marietou-GUEYE <gueye.etou@gmail.com>
2025-08-09 23:08:47 +0200

    modif .env

--- app/__init__.py
+++ app/__init__.py
@@ -37,9 +37,9 @@ def as_bool(name, default=False):
 # --- Config MAIL ---
 
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = as_bool('MAIL_USE_TLS', True)
-app.config['MAIL_USE_SSL'] = as_bool('MAIL_USE_SSL', False)
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 465))
+app.config['MAIL_USE_TLS'] = as_bool('MAIL_USE_TLS', False)
+app.config['MAIL_USE_SSL'] = as_bool('MAIL_USE_SSL', True)
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
